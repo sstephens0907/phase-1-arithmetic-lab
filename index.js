d6cbc516c18b0f257920326a3c4543ec872f5bb7
Sarah Stephens <sarahstephens84@gmail.com>
2022-09-22 17:49:20 -0400

    commit tree c840420aa674fecc89f13d7efb6b3a7b5143e5d4

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+/*const sixtyTwo = function(num1, num2){
+    return num1 * num2
+}
+let multiply = sixtyTwo(31,2)*/
+
+/*function multiply(num1, num2) {
+    num1 * num2
+    return num1 * num2
+}*/
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+
+const random = (Math.floor(Math.random() * 10))
+
+const num3 = 40
+const num4 = 6
+const mod = num3 % num4
+
+const max = Math.max(14, 15, 16, 17, 18, 19, 20)
